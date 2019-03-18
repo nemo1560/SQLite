72191c5a689f9c615d3b6c39e35d45e7ac9eef7d
nemo1560 <signature8100@gmail.com>
2019-03-18 21:56:40 +0700

    build xong CRUD class

---
+++ app/src/main/java/com/example/nemo1/sqliteexample/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.nemo1.sqliteexample;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nemo1.sqliteexample.Entity.Note;
+import com.example.nemo1.sqliteexample.Interface.SendData;
+import com.example.nemo1.sqliteexample.View.ControlFragment;
+import com.example.nemo1.sqliteexample.View.MainFragment;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SendData {
+    private FragmentTransaction fragmentTransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initFragment();
+    }
+
+    private void initFragment() {
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_layout,new MainFragment());
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onSearchId(int id) {
+
+    }
+
+    @Override
+    public void onCallBack() {
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_layout,new ControlFragment());
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onSendResult(String result) {
+
+    }
+
+    @Override
+    public void onSendListResult(List<Note> noteList) {
+
+    }
+
+    @Override
+    public void onSendSingleResult(Note note) {
+
+    }
+}
